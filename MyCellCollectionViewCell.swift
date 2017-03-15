d4600636127f009b7b5061974f51b48960be2aad
SiyuanGuo <guosiyuan@berkeley.edu>
2017-03-15 00:30:21 -0700

    finish the lab

---
+++ MyCellCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCellCollectionViewCell.swift
+//  PokedexLab
+//
+//  Created by siyuan on 3/2/17.
+//  Copyright © 2017 iOS Decal. All rights reserved.
+//
+
+import UIKit
+
+class MyCellCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ PokedexLab/MyTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableCell.swift
+//  PokedexLab
+//
+//  Created by siyuan on 3/9/17.
+//  Copyright © 2017 iOS Decal. All rights reserved.
+//
+
+import UIKit
+
+class MyTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
